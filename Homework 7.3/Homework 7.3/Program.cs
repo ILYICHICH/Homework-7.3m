e45eb21673a4f7f216e23ae3f705b5df7ede3d19
ILYICHICH <36589196+ILYICHICH@users.noreply.github.com>
2018-03-02 18:26:15 +0200

    Add files via upload

---
+++ Homework 7.3/Homework 7.3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_7._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 4, 3, 2, 1 };
+
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+
+            }
+            Console.WriteLine(' ');
+            array[0] = 1;
+            array[1] = 2;
+            array[2] = 3;
+            array[3] = 4;
+            array[4] = 5;
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+
+            }
+        }
+    }
+}
